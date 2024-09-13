02b0a39550c6561c8a3ae40a897bfb29881b2f6b
hajzok <hajzok45@gmail.com>
2024-09-12 22:32:10 -0300

    .

--- DS - 3º Semestre/Desenvolvimento de Sistemas II/Atividade/test_math_functions.py
+++
@@ -1,7 +0,0 @@
-from math_functions import multiply
-
-def test_multiply():
-    assert multiply(2, 3) == 6
-    assert multiply(-1, 5) == -5
-    assert multiply(0, 10) == 0
-    assert multiply(-3, -4) == 12
