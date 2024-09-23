1a8351f5ad381f6afb9c879a0ae84af10c3049e9
hajzok <hajzok45@gmail.com>
2024-09-23 19:51:39 -0300

    .

--- DS - 3º Semestre/Desenvolvimento de Sistemas II/teste.py
+++ DS - 3º Semestre/Desenvolvimento de Sistemas II/teste.py
@@ -1,5 +1,15 @@
-def soma (x, y):
-    return x + y
+def multiplicar (x, y):
+    return x * y
 
-def test_soma():
-    assert 5 == soma(2,3)
+def test_multiplicacao_positivos():
+    assert 6 == multiplicar(2,3)
+
+def test_multiplicacao_negativos():
+    assert 6 == multiplicar(-2,-3)
+
+def test_multiplicacao_positivo_com_negativos():
+    assert -6 == multiplicar(2,-3)
+
+def test_multiplicacao_zero():
+    assert 0 == multiplicar(0,3)
+    assert 0 == multiplicar(3,0)
